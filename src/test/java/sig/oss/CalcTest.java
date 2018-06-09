e6212a01157c026887dc9ee69ea6638fbca49251
khs9962 <40079047+khs9962@users.noreply.github.com>
2018-06-09 12:00:41 +0900

    Update CalcTest.java

--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -7,9 +7,16 @@ public class CalcTest {
 		Calc c = new Calc();
 		assertEquals(30, c.add(10, 20));
 	}
-
+	@Test
 	public void testSub() {
 		Calc c = new Calc();
 		assertEquals(-10, c.sub(10, 20));
+	}
+	@Test
+	public void testMultipleInc() {
+		Calc c = new Calc();
+		c.inc(25);
+		c.inc(25);
+		assertEquals(50, c.getResult());
 	}
 }
