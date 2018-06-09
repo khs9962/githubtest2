59d47a5e1ab5e2a7da43cb4d2a5d1c727d9911b6
khs9962 <40079047+khs9962@users.noreply.github.com>
2018-06-09 11:58:32 +0900

    Update Calc.java

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,5 +1,8 @@
 public class Calc 
 {
+    private int r = 0;
     public int add(int x, int y) { return x+y; }
     public int sub(int x, int y) { return x-y; }
+    public int inc(int d) { r+=d; }
+    public int getResult() { return r; }
 }
